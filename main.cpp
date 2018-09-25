760986e5acb25b5fd4099fc4285bf87f6a60c128
ootomoh <ootomo.h@rio.gsic.titech.ac.jp>
2018-09-25 15:38:05 +0900

    mod : add help option

--- main.cpp
+++ main.cpp
@@ -3,7 +3,12 @@
 
 const std::string cs = "sub";
 
-int main(){
+int main(int argc, char** argv){
+	if(argc > 1 && std::string("-h").compare(argv[1]) == 0){
+		std::cout<<"reboot-f help"<<std::endl
+			<<"See https://github.com/ootomoh/reboot-f"<<std::endl;
+		return 0;
+	}
 	std::cout<<"-- REBOOT --"<<std::endl;
 	std::cout<<"Press Ctrl-C after some minutes"<<std::endl;
 	for(auto c : cs){
