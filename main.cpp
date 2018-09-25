a861f01d66cd99208ca5702486895ab77a689362
ootomoh <ootomo.h@rio.gsic.titech.ac.jp>
2018-09-25 15:20:08 +0900

    mod : add message

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <fstream>
+#include <iostream>
 
 const std::string cs = "sub";
 
 int main(){
+	std::cout<<"-- REBOOT --"<<std::endl;
+	std::cout<<"Press Ctrl-C after some minutes"<<std::endl;
 	for(auto c : cs){
 		std::ofstream ofs("/proc/sysrq-trigger");
 		ofs<<c;
