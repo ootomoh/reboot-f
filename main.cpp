8ac2311dadfe133b3f92a10a859ef714578a10c2
ootomoh <ootomo.h@rio.gsic.titech.ac.jp>
2018-10-24 14:04:19 +0900

    add log recording

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <unistd.h>
 
 const std::string cs = "sub";
 
@@ -11,6 +12,19 @@ int main(int argc, char** argv){
 	}
 	std::cout<<"-- REBOOT --"<<std::endl;
 	std::cout<<"Press Ctrl-C after some minutes"<<std::endl;
+
+	constexpr std::size_t buffer_size = 256;
+	char username[buffer_size];
+	char hostname[buffer_size];
+	getlogin_r(username, buffer_size);
+	gethostname(hostname, buffer_size);
+	std::cout<<username<<" : "<<hostname<<std::endl;
+
+	std::ofstream log_ofs("/home/user/reboot-f.log", std::ios::app);
+	log_ofs << username <<" ,"<< hostname << "\n";
+	log_ofs.close();
+
+
 	for(auto c : cs){
 		std::ofstream ofs("/proc/sysrq-trigger");
 		ofs<<c;
